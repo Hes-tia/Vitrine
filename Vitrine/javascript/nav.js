94c64122b40fdc204ecf39f2200f86bf180ea612
Hes-tia <150133497+Hes-tia@users.noreply.github.com>
2024-11-10 22:49:52 +0100

    Add files via upload

---
+++ Vitrine/javascript/nav.js
@@ -0,0 +1,27 @@
+//var
+let navbutton=document.getElementById("triggerbutton")
+let nav=document.getElementById("nav")
+let main=document.getElementById("main")
+
+//var utiles
+
+let isHidden = true
+
+//function
+function show(){
+    if(isHidden){
+    nav.style.left="0vw" 
+    triggerbutton.style.left="15vw"
+    //main.style.left="15vw"
+    isHidden = false
+    }
+    else{
+    nav.style.left="-15vw"
+    triggerbutton.style.left="0vw"
+    //main.style.left="7.5vw"
+    isHidden = true
+    }
+}
+
+//trigger
+navbutton.addEventListener("click", show)
